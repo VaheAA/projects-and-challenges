12da6a61e5dce75165d3a145a44e6adc8540b055
Vahe Abovyan <vahe.abov@gmail.com>
2023-02-12 20:47:59 +0400

    finish backend

--- mern-projects/jobs/backend/app.js
+++ mern-projects/jobs/backend/app.js
@@ -10,6 +10,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const app = express();
 
 // error handler
@@ -29,6 +34,13 @@ app.use(cors());
 app.use(xss());
 // extra packages
 
+app.get('/', (req, res) => {
+  res.send(
+    `<h1>Jobs API</h1> <br /> <a href="/api-docs">Explore Documentation</a>`
+  );
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authMiddleware, jobsRouter);
